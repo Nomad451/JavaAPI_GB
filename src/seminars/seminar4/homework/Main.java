e5991252b38599b1cccdda2f9ec579fe11a88812
Nomad <yakushev_v@rambler.ru>
2023-06-24 15:57:00 +0300

    completed homework

---
+++ src/seminars/seminar4/homework/Main.java
@@ -0,0 +1,30 @@
+package seminars.seminar4.homework;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+
+        while (true) {
+            String word = sc.nextLine();
+            list.add(0, word);
+            if (word.equals("print")) {
+                list.removeFirst();
+                System.out.println(list);
+            }
+            if (word.equals("revert")) {
+                list.removeFirst();
+                list.removeFirst();
+                System.out.println(list);
+            }
+            if (word.equals("exit")) break;
+        }
+
+    }
+
+}
